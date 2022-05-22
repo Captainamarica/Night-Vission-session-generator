5572b6c556f997d39f535566833605e570bfd62d
Navanjana <104069319+Captainamarica@users.noreply.github.com>
2022-05-22 13:19:49 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,23 +2,23 @@ from pyrogram.types import InlineKeyboardButton
 
 
 class Data:
-    generate_single_button = [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")]
+    generate_single_button = [InlineKeyboardButton(" Start Generating Session ", callback_data="generate")]
 
     home_buttons = [
         generate_single_button,
-        [InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")]
+        [InlineKeyboardButton(text=" Return Home ", callback_data="home")]
     ]
 
     generate_button = [generate_single_button]
 
     buttons = [
         generate_single_button,
-        [InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/SLBotOfficial/28")],
+        [InlineKeyboardButton(" Bot Status and More Bots ", url="https://t.me/NightVission")],
         [
-            InlineKeyboardButton("How to Use ❔", callback_data="help"),
-            InlineKeyboardButton("🎪 About 🎪", callback_data="about")
+            InlineKeyboardButton("How to Use ", callback_data="help"),
+            InlineKeyboardButton(" About ", callback_data="about")
         ],
-        [InlineKeyboardButton("Code Owner", url="https://t.me/ImDark_Empire") , InlineKeyboardButton("DEVOLOPER TEAM", url="https://t.me/SlBots_Admins")],
+        [InlineKeyboardButton("Code Owner", url="https://t.me/NA_VA_N_JA_NA1") , InlineKeyboardButton("DEVOLOPER TEAM", url="https://t.me/NightVission")],
     ]
 
     START = """
@@ -33,7 +33,7 @@ If you don't trust this bot,
 Still reading?
 You can use me to generate pyrogram (even version 2) and telethon string session. Use below buttons to learn more !
 
-By @SLBotOfficial
+By @NightVission
     """
 
     HELP = """
@@ -50,13 +50,13 @@ By @SLBotOfficial
     ABOUT = """
 **About This Bot** 
 
-Telegram Bot to generate Pyrogram and Telethon string session by @SLBotOfficial
+Telegram Bot to generate Pyrogram and Telethon string session by @NA_VA_N_JA_NA1
 
-Source Code : [Click Here](https://github.com/DARKEMPIRESL/StringSessionBot)
+Source Code : [Click Here](https://github.com/Captainamarica/Night-Vission-session-generator)
 
 Framework : [Pyrogram](https://docs.pyrogram.org)
 
 Language : [Python](https://www.python.org)
 
-Developer : @SL_BOTS_TM
+Developer : @NightVission
     """
